e17868e263ab47e3170ccb166c0149e57ba14770
Sobhan Akhtar <sobhanakhtar16@gmail.com>
2023-05-10 13:56:21 +0100

    Added mongodb access, login and register logic

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from flask import Flask, session, request
+from flask import Flask, session, request, flash, render_template
+from pymongo import MongoClient
+import certifi
 
 app = Flask(__name__)
 app.secret_key = 'fUjXn2r5u8x!A%D*G-KaPdSgVkYp3s6v'
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://TRP:ZivIortmj3k8olHW@cluster0.uw2qkum.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+
+try:
+    client.admin.command('ping')
+    print("Successfully pinged db")
+    database = client['TRB']
+except Exception as e:
+    print(e)
 
 @app.route("/")
 def loadIndexPage():
@@ -12,18 +24,52 @@ def displayLoginPage():
     if request.method == "GET":
         return "Login Page"
     else:
-        pass
+        username = request.form.get('username')
+        password = request.form.get('password')
 
+        user_db = database['users']
+        # Check user exists
+        user = user_db.find_one({'username': username})
+        if user:
+            if user['password'] == password:
+                session['username'] = username
+                session['logged_in'] = True
+                print("testing111")
+            
+                return "Successfully Logged in"
+            return "Incorrect credentials, try again"
+        else:
+            return "This Account does not exist"
+    
 @app.route("/register", methods=["GET", "POST"])
 def displayRegisterPage():
     if request.method == "GET":
         return "Register Page"
     else:
-        pass
+        username = request.form.get('username')
+        password = request.form.get('password')
+        email = request.form.get('email')
+
+        user_db = database['users']
+        # Check if user already exists
+        user_status = user_db.find_one({"username": username})
+
+        if user_status:
+            return "this user already exists"
+
+        user = {'username': username, 'password': password, 'email': email}
+        x = user_db.insert_one(user)
+
+        return "Successfully Registered"
+    
 
 @app.route("/create_recipe", methods=["POST"])
 def createRecipe():
-    
+    if session["username"] and session['logged_in']:
+        recipe_name = request.form.get('name')
+        
+
+
 
 
 
